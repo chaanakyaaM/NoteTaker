ee82b1295196a73296507993b9fc4ca47af38c76
chaanakyaaM <chaanakyaam@gmail.com>
2024-10-18 19:29:15 +0530

    local storge working perfectly and added the layout for search feature

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,19 @@
-import React from "react";
+import React,{useState} from "react";
 import { NavLink } from "react-router-dom";
 
 export default function Navbar({ logo, add, setadd, len }) {
   function clickHandler() {
     setadd(!add);
   }
+  const [searchval, setsearchval] = useState('')
+  
 
   return (
     <div className="navbar">
       <NavLink to="/">
         <div className="logo">{logo}</div>
       </NavLink>
+      <input type="text" className='search' placeholder='Search' onChange={(e)=>setsearchval(e.target.val)} value={searchval} />
       <div className="links">
         <NavLink to="/">
           <button className="buttons">Home</button>
@@ -20,9 +23,9 @@ export default function Navbar({ logo, add, setadd, len }) {
             Create Blog
           </button>
         </NavLink>
-        <NavLink to="/about">
+        {/* <NavLink to="/about">
           <button className="buttons">About</button>
-        </NavLink>
+        </NavLink> */}
         <div className="len">Total Posts: {len}</div>
       </div>
     </div>
